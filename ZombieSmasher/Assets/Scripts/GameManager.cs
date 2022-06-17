15da4929e1fb8d868f8d8a4624965c22aae15b86
Fredy <jfr3dy90@gmail.com>
2022-06-17 17:54:01 -0500

    Start Game Structure

---
+++ ZombieSmasher/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameState actualGameState;
+
+    private void Start()
+    {
+        OnLaunchGame(actualGameState);
+    }
+
+    void OnLaunchGame(GameState gs)
+    {
+        switch (gs)
+        {
+            case GameState.MainMenu:
+
+                break;
+            case GameState.Play:
+                WaveController.Instance.
+                break;
+            case GameState.GameOver:
+
+                break;
+        }
+    }
+
+}
+public enum GameState
+{ 
+    MainMenu,
+    Play,
+    GameOver
+}
---
+++ ZombieSmasher/Assets/Scripts/PoolManager.cs
@@ -0,0 +1,13 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    [SerializeField] GameObject pref_zombie;
+    [SerializeField] GameObject pref_human;
+    [SerializeField] GameObject pref_esqueleton;
+
+
+}
